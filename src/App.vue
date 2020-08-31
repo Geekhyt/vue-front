1fbb6c28faaadb3d6bf2cde1fbe6b833eed37438
霍怡童 <huoyt@csdn.net>
2020-09-01 01:23:57 +0800

    dev: dev

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul id="app">
+    <li v-for="(user) in users" :key="user.id">
+        {{user.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+      return {
+          users: []
+      }
+  },
+  mounted() {
+      axios.get('http://localhost:3000/api/users').then(response => {
+          this.users = response.data;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
